fc31dc3c494783c6546fcc91458182d51d970cbd
Mas Al <aldo@mifx.com>
2024-07-10 17:55:01 +0700

    feat: add igbinary ext enable for default module when install on cygwin

--- sapi/scripts/cygwin/cygwin-config-ext.sh
+++ sapi/scripts/cygwin/cygwin-config-ext.sh
@@ -17,6 +17,7 @@ REDIS_VERSION=6.0.2
 MONGODB_VERSION=1.19.3
 YAML_VERSION=2.2.3
 IMAGICK_VERSION=3.7.0
+IGBINARY_VERSION=3.2.15
 
 if [ ! -d pool/ext ]; then
     mkdir -p pool/ext
@@ -54,6 +55,14 @@ if [ ! -d $ROOT/ext/imagick ]; then
     fi
     tar xvf imagick-${IMAGICK_VERSION}.tgz
     mv imagick-${IMAGICK_VERSION} $ROOT/ext/imagick
+fi
+
+if [ ! -d $ROOT/ext/igbinary ]; then
+    if [ ! -f igbinary-${IGBINARY_VERSION}.tgz ]; then
+        wget https://pecl.php.net/get/igbinary-${IGBINARY_VERSION}.tgz
+    fi
+    tar xvf igbinary-${IGBINARY_VERSION}.tgz
+    mv igbinary-${IGBINARY_VERSION} $ROOT/ext/igbinary
 fi
 
 cd $ROOT
